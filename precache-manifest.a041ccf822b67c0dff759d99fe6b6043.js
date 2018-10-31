eaa5a0a9a813526df9d3adb330aef469888e5892
Steve Griffith <griffis@algonquincollege.com>
2018-10-31 13:41:40 -0400

    Updates

---
+++ precache-manifest.a041ccf822b67c0dff759d99fe6b6043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreponame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e0ae0b369ffcc5fa2160",
+    "url": "/myreponame/static/js/runtime~main.e0ae0b36.js"
+  },
+  {
+    "revision": "74e55c96aa390412205d",
+    "url": "/myreponame/static/js/main.74e55c96.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/myreponame/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "74e55c96aa390412205d",
+    "url": "/myreponame/static/css/main.3aededde.chunk.css"
+  },
+  {
+    "revision": "52bee8ca00edc7b9e02bee29d6263012",
+    "url": "/myreponame/index.html"
+  }
+];
